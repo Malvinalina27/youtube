63497fee60224f1e543ee27178ee1df6cc75fcfc
Malvinalina27 <rasberry-07@mail.ru>
2021-04-21 15:50:25 +0300

    переключение слайдов отдельно у каждого раздела, кнопка subscribe

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const myChannelSlider = new Swiper('.my-channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '#my-channel-button-next',
+    prevEl: '#my-channel-button-prev',
+  },
+});
+const recommendedSlider = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+  // Navigation arrows
+  navigation: {
+    nextEl: '#recommended-button-next',
+    prevEl: '#recommended-button-prev',
+  },
+});
+const recommendedChannelSlider = new Swiper('.recommended-channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '#recommended-channel-button-next',
+    prevEl: '#recommended-channel-button-prev',
+  },
+});
