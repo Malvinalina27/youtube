16debf2733f88231b02fa6b5f3ad777b1c2f5677
Malvinalina27 <rasberry-07@mail.ru>
2021-04-21 17:44:59 +0300

    адаптивность

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 const myChannelSlider = new Swiper('.my-channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '#my-channel-button-next',
@@ -11,7 +29,16 @@ const myChannelSlider = new Swiper('.my-channel-slider', {
 const recommendedSlider = new Swiper('.recommended-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 3,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '#recommended-button-next',
@@ -21,10 +48,40 @@ const recommendedSlider = new Swiper('.recommended-slider', {
 const recommendedChannelSlider = new Swiper('.recommended-channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1350: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    820: {
+      slidesPerView: 2,
+    },
+  },
   // Navigation arrows
   navigation: {
     nextEl: '#recommended-channel-button-next',
     prevEl: '#recommended-channel-button-prev',
   },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('is-open');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  myChannelSlider.destroy();
+  recommendedSlider.destroy();
+  recommendedChannelSlider.destroy();
+}
